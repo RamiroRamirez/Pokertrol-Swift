bbbe350131050d12501171f439612c13d304a2f5
Ramiro Ramirez <ramiro.ramirez@smartmobilefactory.com>
2016-12-24 19:16:06 -0600

    Adding view for torunament maker with enums and cells

---
+++ Pokertrol/Views/PTTournamentMakerTitleCell.swift
@@ -0,0 +1,28 @@
+//
+//  PTTournamentMakerTitleCell.swift
+//  Pokertrol
+//
+//  Created by Ramiro Ramirez on 24/12/2016.
+//  Copyright © 2016 ramram. All rights reserved.
+//
+
+import UIKit
+
+class PTTournamentMakerTitleCell			: UITableViewCell {
+
+	// MARK: - Outlets
+
+	@IBOutlet private weak var titleLabel	: UILabel?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
